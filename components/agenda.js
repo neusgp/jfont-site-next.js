0681945fce40efeb6f6fa7fbfd3830a0390fdd92
NEUS GARCIA <neuspiano@gmail.com>
2022-07-10 12:19:23 +0200

    calendar implementation

--- components/agenda.js
+++ components/agenda.js
@@ -1,27 +1,28 @@
 import { useEffect, useState } from "react";
-
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
 import styles from "../styles/agenda.module.css";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 
 /* moment.locale("en-GB"); */
 
 export default function Agenda() {
     const [eventsList, setEventList] = useState([]);
-    /*  const localizer = momentLocalizer(moment); */
-    /* 
+    const localizer = momentLocalizer(moment);
     useEffect(() => {});
- */
+
     return (
         <div id="agenda" className={styles.agenda}>
             <h1 className={styles.title}>AGENDA</h1>
             <div className={styles.events}>
                 <NextEvents />
                 <div className={styles.calendar}>
-                    {/* <Calendar
+                    <Calendar
                         localizer={localizer}
                         events={eventsList}
                         startAccessor="start"
                         endAccessor="end"
-                    /> */}
+                    />
                 </div>
             </div>
         </div>
