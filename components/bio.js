09f60b8e2a533111f578c7c5e5ae657b0ce9672e
NEUS GARCIA <neuspiano@gmail.com>
2022-07-23 11:03:20 +0200

    css

--- components/bio.js
+++ components/bio.js
@@ -5,18 +5,21 @@ export default function Bio() {
     return (
         <div className={styles.bio}>
             <div id="bio" className={styles.content}>
-                <h1 className={styles.title}>BIO</h1>
-                <p className={styles.text}>
-                    Joaquim Font (Igualada, 1995) es forma en els seus inicis al
-                    conservatori dIgualada. Posteriorment, becat per la fundació
-                    de música Ferrer-Salat es gradua al conservatori del Liceu
-                    amb Josep Colom i Raimon Garriga. Des del 2018 resideix a
-                    Essen (Alemanya) on estudia amb Hisako Kawamura i Lied i
-                    correpetició dòpera amb Xaver Poncette a la Folkwang
-                    Universität der Kunste on és becat per la fundació Yehundi
-                    Menuhin. Actua regularment com a solista, música de cambra i
-                    Lied a Espanya i Alemanya.
-                </p>
+                <div className={styles.text}>
+                    <h1 className={styles.title}>BIO</h1>
+                    <p className={styles.biotext}>
+                        Joaquim Font (Igualada, 1995) es forma en els seus
+                        inicis al conservatori dIgualada. Posteriorment, becat
+                        per la fundació de música Ferrer-Salat es gradua al
+                        conservatori del Liceu amb Josep Colom i Raimon Garriga.
+                        Des del 2018 resideix a Essen (Alemanya) on estudia amb
+                        Hisako Kawamura i Lied i correpetició dòpera amb Xaver
+                        Poncette a la Folkwang Universität der Kunste on és
+                        becat per la fundació Yehundi Menuhin. Actua regularment
+                        com a solista, música de cambra i Lied a Espanya i
+                        Alemanya.
+                    </p>
+                </div>
             </div>
         </div>
     );
--- components/footer.js
+++ components/footer.js
@@ -1,13 +1,9 @@
 import React from "react";
-import Link from "next/link";
 
 export default function Footer() {
     return (
         <div id="footer">
             <p>Joaquim Font - Neus Garcia 2022</p>
-            <Link href={"/admin"}>
-                <a>Admin</a>
-            </Link>
         </div>
     );
 }
