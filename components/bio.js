391102fd2ae69e39d66273214d21702189873959
NEUS GARCIA <neuspiano@gmail.com>
2022-07-25 12:47:12 +0200

    css

--- components/bio.js
+++ components/bio.js
@@ -8,10 +8,21 @@ export default function Bio() {
                 <div className={styles.text}>
                     <h1 className={styles.title}>BIO</h1>
                     <p className={styles.biotext}>
-                        Joaquim Font (Igualada, 1995) es forma en els seus
-                        inicis al conservatori dIgualada. Posteriorment, becat
-                        per la fundació de música Ferrer-Salat es gradua al
-                        conservatori del Liceu amb Josep Colom i Raimon Garriga.
+                        <b>Joaquim Font (Igualada, 1995)</b> es forma en els
+                        seus inicis al conservatori dIgualada. Posteriorment,
+                        becat per la fundació de música Ferrer-Salat es gradua
+                        al conservatori del Liceu amb Josep Colom i Raimon
+                        Garriga. Des del 2018 resideix a Essen (Alemanya) on
+                        estudia amb Hisako Kawamura i Lied i correpetició dòpera
+                        amb Xaver Poncette a la Folkwang Universität der Kunste
+                        on és becat per la fundació Yehundi Menuhin. Actua
+                        regularment com a solista, música de cambra i Lied a
+                        Espanya i Alemanya. Joaquim Font (Igualada, 1995) es
+                        forma en els seus inicis al conservatori dIgualada.
+                        Posteriorment, becat per la fundació de música
+                        Ferrer-Salat es gradua al conservatori del Liceu amb
+                        Josep Colom i Raimon Garriga. <br />
+                        <br />
                         Des del 2018 resideix a Essen (Alemanya) on estudia amb
                         Hisako Kawamura i Lied i correpetició dòpera amb Xaver
                         Poncette a la Folkwang Universität der Kunste on és
--- components/media.js
+++ components/media.js
@@ -1,54 +1,15 @@
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
+import Image from "next/image";
 import styles from "../styles/media.module.css";
-import ImageGallery from "react-image-gallery";
 import "react-image-gallery/styles/css/image-gallery.css";
-
-const images = [
-    {
-        original:
-            "https://joaquimfont.s3.eu-central-1.amazonaws.com/JOAQUIM+F.+17.jpg",
-    },
-    {
-        original:
-            "https://joaquimfont.s3.eu-central-1.amazonaws.com/JOAQUIM+F.+28.jpg",
-    },
-    {
-        original:
-            "https://joaquimfont.s3.eu-central-1.amazonaws.com/JOAQUIM+F.+33.jpg",
-    },
-    {
-        original:
-            "https://joaquimfont.s3.eu-central-1.amazonaws.com/Joaquim1.jpg",
-    },
-    {
-        original:
-            "https://joaquimfont.s3.eu-central-1.amazonaws.com/Joaquim29.jpg",
-    },
-    {
-        original:
-            "https://joaquimfont.s3.eu-central-1.amazonaws.com/Joaquim42.jpg",
-    },
-    {
-        original:
-            "https://joaquimfont.s3.eu-central-1.amazonaws.com/Joaquim44.jpg",
-    },
-    {
-        original:
-            "https://joaquimfont.s3.eu-central-1.amazonaws.com/Joaquim53.jpg",
-    },
-];
 
 export default function Media() {
     return (
         <div id="media" className={styles.media}>
-            <div className={styles.gallery}>
-                <ImageGallery
-                    items={images}
-                    slideDuration={1200}
-                    slideInterval={6000}
-                    autoPlay={true}
-                />
-            </div>
+            <h1 className={styles.title}>MEDIA</h1>
+
+            <Gallery />
             <div className={styles.video}>
                 <iframe
                     className={styles.iframe}
@@ -70,6 +31,64 @@ export default function Media() {
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowFullScreen
                 ></iframe>
+            </div>
+        </div>
+    );
+}
+
+function Gallery() {
+    return (
+        <div className={styles.gallery}>
+            <div className={styles.column1}>
+                <img
+                    className={styles.img}
+                    src="https://joaquimfont.s3.eu-central-1.amazonaws.com/JOAQUIM+F.+17.jpg"
+                    alt="Joaquim Font"
+                />
+                <img
+                    className={styles.img}
+                    src="https://joaquimfont.s3.eu-central-1.amazonaws.com/JOAQUIM+F.+28.jpg"
+                    alt="Joaquim Font"
+                />
+                <img
+                    className={styles.img}
+                    src="https://joaquimfont.s3.eu-central-1.amazonaws.com/JOAQUIM+F.+33.jpg"
+                    alt="Joaquim Font"
+                />
+            </div>
+            <div className={styles.column2}>
+                <img
+                    className={styles.img}
+                    src="https://joaquimfont.s3.eu-central-1.amazonaws.com/Joaquim1.jpg"
+                    alt="Joaquim Font"
+                />
+                <img
+                    className={styles.img}
+                    src="https://joaquimfont.s3.eu-central-1.amazonaws.com/Joaquim29.jpg"
+                    alt="Joaquim Font"
+                />
+                <img
+                    className={styles.img}
+                    src="https://joaquimfont.s3.eu-central-1.amazonaws.com/Joaquim42.jpg"
+                    alt="Joaquim Font"
+                />
+            </div>
+            <div className={styles.column3}>
+                <img
+                    className={styles.img}
+                    src="https://joaquimfont.s3.eu-central-1.amazonaws.com/Joaquim44.jpg"
+                    alt="Joaquim Font"
+                />
+                <img
+                    className={styles.img}
+                    src="https://joaquimfont.s3.eu-central-1.amazonaws.com/Joaquim53.jpg"
+                    alt="Joaquim Font"
+                />
+                {/*  <img
+                    className={styles.img}
+                    src="https://joaquimfont.s3.eu-central-1.amazonaws.com/Joaquim53.jpg"
+                    alt="Joaquim Font"
+                /> */}
             </div>
         </div>
     );
